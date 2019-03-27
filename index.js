319bd553377bf22d0770a970dda3f99902d9a628
Frank Martinez <frankie.martinez@gmail.com>
2019-03-27 10:33:53 -0700

    commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
+
+
 const knexConfig = {
   client: 'sqlite3',
   connection: {
---
+++ migrations/20190327102831_roles_table.js
@@ -0,0 +1,9 @@
+// What new changes we need to make
+exports.up = function(knex, Promise) {
+  
+};
+
+// how to undo the changes made in the up function
+exports.down = function(knex, Promise) {
+  
+};
